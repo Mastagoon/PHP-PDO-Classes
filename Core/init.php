4f3d49931738d52a1b1cecb601f5a83a828955e3
sahal alzubair <inzilagiway@gmail.com>
2019-10-28 14:40:08 +0300

    added comments

--- Core/init.php
+++ Core/init.php
@@ -1,22 +1,21 @@
 <?php
-  session_start();
-
-  $GLOBALS['config'] = array(
+/*
+The Init.php file holds all the global variables needed in this project(the database connection only so far)
+It also gets an instance of the database which means you are always connected when you require Core/init.php
+It also automatically loads the classes using spl_autoload function
+*/
+  $GLOBALS['config'] = array(	//global variable config equals this array
     'mysql' => array(
-      'host' => '',
-      'username' => '',
+      'host' => '127.0.0.1',
+      'username' => 'root',
       'password' => '',
-      'dbname' =>   ''
-    ),
-    'seession' => array (
-      'session_name' => '',
-      'token_name' => ''
-    )
+      'dbname' =>   'application_db'
+	)
   );
 
-  spl_autoload_register(function($class){
+  spl_autoload_register(function($class){	//this function automatically loads the given class ONLY when it's called, when you use Input::get() this function requires it immediately.
     require_once "Classes/" . $class . ".php";
   });
 
   DB::getInstance();
-  require_once "Functions/escape.php";
+  require_once "Functions/escape.php";	
